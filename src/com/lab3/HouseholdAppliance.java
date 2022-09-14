a083b629891cefded87dcfe74f97453f8b6910da
NazariiSenkiv <95177352+NazariiSenkiv@users.noreply.github.com>
2022-09-14 12:24:53 +0300

    Initial commit

---
+++ src/com/lab3/HouseholdAppliance.java
@@ -0,0 +1,29 @@
+package com.lab3;
+
+public class HouseholdAppliance {
+    private String name;
+    private String model;
+    private String manufacturer;
+    private Float price;
+    private Float maxPower;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public Float getMaxPower() {
+        return maxPower;
+    }
+}
